8f1e9acf7026c79ff0f23e237a85e7991e0d3a29
Srushti Gokhe <srushtigokhe200@gmail.com>
2026-01-23 12:19:48 +0530

    Revert "Initial Commit"
    This reverts commit 46ffbc472345522349e9e71197f87d59e02e0cd6.

--- src/hooks/useLogin.js
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { loginUser } from "../services/authService";
-
-export const useLogin = () => {
-  return useMutation({
-    mutationFn: loginUser,
-  });
-};
--- src/utils/api_constants.js
+++
@@ -1,4 +0,0 @@
-export const API_ROUTES = {
-    login: "auth/login",
-    getMe: "auth/me",
-}
