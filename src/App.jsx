b27db12ceabb75f36a715aac97d05d773d1847dc
Srushti Gokhe <srushtigokhe2004@gmail.com>
2026-02-13 11:54:04 +0530

    fix refetch (#13)
    Co-authored-by: Srushti Gokhe <srushtigokhe200@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import Login from './pages/Login'
 import AppRoutes from './routes/AppRoutes'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { CacheProvider } from '@emotion/react';
 import rtlPlugin from 'stylis-plugin-rtl';
 import createCache from "@emotion/cache";
@@ -9,15 +10,6 @@ import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from 'react-router-dom';
 import "./index.css"
-
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 const emotionCacheOptions = {
   rtl: {
@@ -38,11 +30,11 @@ function App() {
       <CacheProvider value={createCache(emotionCacheOptions.ltr)}>
         <QueryClientProvider client={queryClient}>
           <AuthProvider>
-              <AppRoutes>
-              </AppRoutes>
+            <AppRoutes>
+            </AppRoutes>
             <Toaster position="top-right" reverseOrder={false} />
-            
-            </AuthProvider>
+
+          </AuthProvider>
         </QueryClientProvider>
       </CacheProvider>
     </>
