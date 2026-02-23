1a295ce2bfb47b29346a1274eda0d657ef1b87e1
Srushti Gokhe <srushtigokhe200@gmail.com>
2026-02-23 17:53:23 +0530

    feat: added completeProfileDialog to Protected routes so it is shown every where until profile is completed

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,12 +1,18 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import CompleteProfileDialog from "../pages/Profile/CompleteProfileDialog";
 
 const ProtectedRoutes = ({ children }) => {
   const { isAuthenticated } = useAuth();
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
-  return children;
+  return (
+    <>
+      <CompleteProfileDialog />
+      {children}
+    </>
+  );
 };
 
 export default ProtectedRoutes;
